e001900520cb2218625d1176f7cd528742ffdd2a
Sebita1809 <sebadiazmoyano@gmail.com>
2024-10-05 13:46:29 -0300

    Finalizado los ejercicios de Condicionales.java

--- Condicionales.java
+++ Condicionales.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.Locale;
 import java.util.Random;
 
 // EJERCICIO N°1 //////////////////////////////////////////////////////////////////////////////////////
@@ -114,25 +115,54 @@ import java.util.Random;
 //    }
 //}
 // EJERCICIO N°6 //////////////////////////////////////////////////////////////////////////////////////
-public class Condicionales {
-    public static void main(String[] args){
-        int horasDeSueño;
-        int horasDeEjercicio;
-        int comidasSaludables;
-        horasDeSueño = Integer.parseInt(JOptionPane.showInputDialog("Cuantas horas son generalmente las que usted duerme?"));
-        horasDeEjercicio = Integer.parseInt(JOptionPane.showInputDialog("Cuantas horas al dia realiza ejercicio?"));
-        comidasSaludables = Integer.parseInt(JOptionPane.showInputDialog("Cuantas comidas saludables en el dia?"));
-        
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
+//public class Condicionales {
+//    public static void main(String[] args){
+//        int horasDeSueño;
+//        int horasDeEjercicio;
+//        int comidasSaludables;
+//        horasDeSueño = Integer.parseInt(JOptionPane.showInputDialog("Cuantas horas son generalmente las que usted duerme?"));
+//        horasDeEjercicio = Integer.parseInt(JOptionPane.showInputDialog("Cuantas horas al dia realiza ejercicio?"));
+//        comidasSaludables = Integer.parseInt(JOptionPane.showInputDialog("Cuantas comidas saludables en el dia?"));
+//
+//        if (horasDeSueño == 8 && horasDeEjercicio > 1 && comidasSaludables > 1){
+//        JOptionPane.showMessageDialog(null, "Vemos que tus habitos son realmente saludables, igualmente, recuerda que darte permitidos está bien");
+//        } else if (horasDeSueño >= 6 || horasDeSueño <= 8 || horasDeEjercicio > 1 || comidasSaludables >= 1){
+//            JOptionPane.showMessageDialog(null, "Usted posee algunos habitos saludables, recomendamos mejorar un poco en aquellos donde no se encuentra tan bien");
+//        } else if (horasDeSueño > 8 || horasDeSueño < 6 || horasDeEjercicio < 1 || comidasSaludables == 0) {
+//            JOptionPane.showMessageDialog(null, "Recomendamos que se realice cambios en sus rutinas, debido a que la vida que lleva no es muy sana");
+//        }
+//        JOptionPane.showMessageDialog(null, "A continuación, le comentaremos sobre como categorizamos los habitos saludables");
+//        JOptionPane.showMessageDialog(null, "Con respecto al sueño, lo mas saludables es dormir 8 hs, pero generalmente\n" +
+//                                                                   "las personas duermen unas 6-7 hs, menos de eso ya no es bueno.\n" +
+//                                                                   "-------------------------------------------------------------------------------\n" +
+//                                                                   "Sobre las horas de ejercicio, lo recomedable es hacer por lo menos 1.5 hs,\n" +
+//                                                                   "por lo general la gente sueles hacer 30 min, pero menos de eso no es saludable. \n" +
+//                                                                   "-------------------------------------------------------------------------------\n" +
+//                                                                   "Finalmente lo recomedable es comer por lo menos 2 comidas saludables, en general\n" +
+//                                                                   "las personas sueles comer 1 comida saludable por dia, menos de eso no es recomedable.");
+//
+//    }
+//}
+// EJERCICIO N°7 //////////////////////////////////////////////////////////////////////////////////////
+//public class Condicionales {
+//    public static void main(String[] args) {
+//        String estadoDeAnimo;
+//        estadoDeAnimo = JOptionPane.showInputDialog("¿Podria indicarnos cual es su estado de animo?").toLowerCase();
+//        switch (estadoDeAnimo){
+//            case "feliz":
+//                JOptionPane.showMessageDialog(null, "Le recomendamos que dibuje, ya que puede lograr hermosas obras de arte");
+//                break;
+//            case "triste":
+//                JOptionPane.showMessageDialog(null, "Le recomendamos juntarse con amigos, los amigos siempre ayudan a sentirse mejor");
+//                break;
+//            case "energico":
+//                JOptionPane.showMessageDialog(null, "Le recomendamos salir a hacer ejercicio, asi aprovecha la energia para algo sano");
+//                break;
+//            case "relajado":
+//                JOptionPane.showMessageDialog(null, "Le recomendamos sentarse a ver una pelicula, hay que aprovechar los momentos de paz");
+//                break;
+//            default:
+//                JOptionPane.showMessageDialog(null, "Lamentablemente no tenemos recomendaciones para su estado de animo actual, mil disculpas");
+//        }
+//    }
+//}
